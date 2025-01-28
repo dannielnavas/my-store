a5b76b078f2512c80748ef6471396677b73dc0fb
Danniel Navas <dannielnavas@gmail.com>
2025-01-28 07:44:28 -0500

    ✨ Agregar Express y configurar un servidor básico en index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("my app");
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
