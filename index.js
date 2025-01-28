35e4ab66f99592a390c47853b8f9360f880f6fb4
Danniel Navas <dannielnavas@gmail.com>
2025-01-28 07:48:00 -0500

    ✨ Agregar nuevas rutas: '/nueva-ruta' y '/products' en index.js

--- index.js
+++ index.js
@@ -8,6 +8,23 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('New endpoint');
+});
+
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: 'Product 1',
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+    },
+  ]);
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
