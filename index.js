56e509222e1de8f3818e67cad96740d815151ed1
Danniel Navas <dannielnavas@gmail.com>
2025-01-30 07:18:11 -0500

    ✨ Refactor rutas: separar lógica de rutas en archivos individuales para usuarios, productos y categorías

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 'use strict';
 
 const express = require('express');
-const { faker } = require('@faker-js/faker');
-
+const routerApi = require('./routes');
 const app = express();
 
 app.get('/', (req, res) => {
@@ -13,55 +12,7 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('New endpoint');
 });
 
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset,
-    });
-  }
-  res.json({
-    message: 'User endpoint',
-  });
-});
-
-app.get('/products', (req, res) => {
-  const { size } = req.query;
-  const limit = size || 10;
-  const products = [];
-  for (let i = 0; i < limit; i++) {
-    products.push({
-      id: i,
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      Image: faker.image.urlPicsumPhotos(200, 200),
-    });
-  }
-  res.json(products);
-});
-
-// Lo especifico debe ir antes de lo dinamico para que no se confunda con el id
-app.get('/products/filter', (req, res) => {
-  res.send('Filter endpoint');
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: `Product ${id}`,
-    price: 100 * id,
-  });
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  });
-});
+routerApi(app);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
---
+++ routes/products.router.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const express = require('express');
+const { faker } = require('@faker-js/faker');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const { size } = req.query;
+  const limit = size || 10;
+  const products = [];
+  for (let i = 0; i < limit; i++) {
+    products.push({
+      id: i,
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      Image: faker.image.urlPicsumPhotos(200, 200),
+    });
+  }
+  res.json(products);
+});
+
+// Lo especifico debe ir antes de lo dinamico para que no se confunda con el id
+router.get('/filter', (req, res) => {
+  res.send('Filter endpoint');
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: `Product ${id}`,
+    price: 100 * id,
+  });
+});
+
+module.exports = router;
