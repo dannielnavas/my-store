bb5a52f5f5cd25cd2198bb98ee727deb2325eb36
Danniel Navas <dannielnavas@gmail.com>
2025-02-04 07:17:45 -0500

    ✨ Renombrar archivo principal a 'api/index.js' y actualizar rutas de la API

--- api/index.js
+++ api/index.js
@@ -25,11 +25,11 @@ const options = {
 };
 app.use(cors(options));
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hello World');
 });
 
-app.get('/nueva-ruta', (req, res) => {
+app.get('/api/nueva-ruta', (req, res) => {
   res.send('New endpoint');
 });
 
