c6d8255cc2ef88d054d4eedc4335c07085c04d99
Danniel Navas <dannielnavas@gmail.com>
2025-02-14 07:42:27 -0500

    ✨ Agregar validación de precios en la búsqueda de productos; permitir filtrado por precio mínimo y máximo

--- api/services/product.service.js
+++ api/services/product.service.js
@@ -1,5 +1,6 @@
 const faker = require('@faker-js/faker');
 const boom = require('@hapi/boom');
+const { Op } = require('sequelize');
 
 const { models } = require('./../lib/sequelize');
 
@@ -29,11 +30,23 @@ class ProductsService {
   async find(query) {
     const options = {
       include: ['category'],
+      where: {},
     };
     const { limit, offset } = query;
     if (limit && offset) {
       options.offset = offset;
       options.limit = limit;
+    }
+    const { price } = query;
+    if (price) {
+      options.where.price = price;
+    }
+    const { price_min, price_max } = query;
+    if (price_min && price_max) {
+      options.where.price = {
+        [Op.gte]: price_min,
+        [Op.lte]: price_max,
+      };
     }
     const products = await models.Product.findAll(options);
     return products;
